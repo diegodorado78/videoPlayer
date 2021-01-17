5323443196738ca528684f7cc8b70748c92e9700
diego dorado <diegodorado78@gmail.com>
2021-01-17 13:08:15 -0500

    setup done Eslint y Sass

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import '../assets/styles/App.scss';
+const HelloWorld = () => {
+    return (
+        <div>
+        <h1> Hello mundo con React </h1>
+        <img src='https://arepa.s3.amazonaws.com/react.png' alt='react-logo'/>
+        </div>
 
-const HelloWorld = () => (
-   <h1>Hello mundo con React oh si </h1>
-);
+    )
+
+}
+
  
 export default HelloWorld;
